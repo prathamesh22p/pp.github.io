82e84ed179ca7bd0dc52c861f45caa5ac3978763
prathamesh22p <64960784+prathamesh22p@users.noreply.github.com>
2021-08-07 17:47:09 +0530

    Portfolio Website

---
+++ index.js
@@ -0,0 +1,7 @@
+const butto = document.querySelector('button');
+// const line = document.querySelector('.line');
+
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark");
+ }
